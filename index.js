0b15f01a61e9eab6bacd25cf34b5a031fcbbe03c
Lee <296241072@qq.com>
2018-06-08 16:26:02 +0800

    移除 js-sha1

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 const Koa = require('koa');
 const Router = require('koa-better-router');
 const staticServer = require('koa-static');
-const sha1 = require('js-sha1');
-var bodyParser = require('koa-bodyparser');
-const bufferEq = require('buffer-equal-constant-time')
+const bodyParser = require('koa-bodyparser');
+const bufferEq = require('buffer-equal-constant-time');
 
 const HOOK_CONFIG = require('./hook.config');
 const PORT = 3000;
 const WEB_PATH = './web/source';
-const crypto = require('crypto');
 
 function sign (secret, data) {
   return 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex')
 }
-
 function verify (signature, secret, data) {
   return bufferEq(Buffer.from(signature), Buffer.from(sign(secret, data)))
 }
@@ -23,13 +21,7 @@ function verify (signature, secret, data) {
 let app = new Koa();
 app.use(bodyParser());
 
-// router
-let router = Router().loadMethods();
-router.get('*', (ctx, next) => {
-  ctx.body = 'Hello Koa2!';
-});
-
-// TODO 自动化脚本 / 更新 web 源码 / 编译源码 /更新 docs 文档
+// 更新 web 源码 / 编译源码 /更新 docs 文档
 let deploy = Router({ prefix: '/deploy' }).loadMethods();
 deploy.post('/doc', (ctx, next) => {
   let signature = ctx.headers['x-hub-signature'];
@@ -53,15 +45,11 @@ deploy.post('/web', (ctx, next) => {
     ctx.body = web();
   }
 });
-deploy.get('/test', (ctx, next) => {
-  //
-  let res = {
-    github: '0d3d61284b8450f3ed8f6bc3200b80a55ccd931e',
-    hex: sha1.hex(HOOK_CONFIG.doc_hook_secret),
-    sha1: sha1(HOOK_CONFIG.doc_hook_secret),
-    crypto: crypto.createHmac('sha1', HOOK_CONFIG.doc_hook_secret).digest('hex')
-  }
-  ctx.body = JSON.stringify(res);
+
+// router
+let router = Router().loadMethods();
+router.get('*', (ctx, next) => {
+  ctx.body = 'Hello Koa2!';
 });
 
 // app.use(staticServer('./web/source/dist'));
